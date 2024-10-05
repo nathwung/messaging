3e32ef7b5bb75f7544186104545f939a58f0a0c8
Nathanael Wung <157310799+nathwung@users.noreply.github.com>
2024-10-05 00:24:44 -0400

    Create App.vue

---
+++ App.vue
@@ -0,0 +1,13 @@
+<template>
+  <router-view />
+</template>
+
+<script setup>
+import { useStore } from 'stores/store' 
+import { onMounted } from 'vue'
+
+onMounted(()=>{
+  const handle = useStore()
+  handle.handleAuthStateChanged()
+})
+</script>
